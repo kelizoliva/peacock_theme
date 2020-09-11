6a9aa15b67431afa41cdb679373aafd7de618cf8
Kristin Snelling <kristin@palantetech.coop>
2020-09-11 00:13:18 -0400

    Style updates, added views infinite scroll template override, js updates

--- js/palante_peacock.js
+++ js/palante_peacock.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
   
   /*$(document).on("scroll", function(){
 		if
@@ -10,5 +10,18 @@ $(document).ready(function () {
 			$(".header-identity-wrapper").removeClass("reduce");
 		}
 	});*/
+
+  // Cache the highest
+  var highestBox = 0;
+  // Select and loop the elements you want to equalise
+  $('.view-display-id-homepage_latest_news_block .views-field-field-main-image').each(function(){
+    // If this box is higher than the cached highest then store it
+    if($(this).height() > highestBox) {
+      highestBox = $(this).height(); 
+    }
+  });
+  // Set the height of all those children to whichever was highest 
+  $('.view-display-id-homepage_latest_news_block .views-field-field-main-image').height(highestBox);
+  $(".view-display-id-homepage_latest_news_block .views-field-field-main-image .field-content").css({"position": "absolute", "bottom": "0"});        
 
 });
