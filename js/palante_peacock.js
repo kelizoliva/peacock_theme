418d64b7054b22f64717e22a5b20dc43ec68a33e
Kristin Snelling <kristin@palantetech.coop>
2020-10-08 03:42:18 -0400

    Lots of style tweaks, js adjustments

--- js/palante_peacock.js
+++ js/palante_peacock.js
@@ -1,15 +1,44 @@
 $(function () {
   
-  /*$(document).on("scroll", function(){
-		if
-      ($(document).scrollTop() > 100){
-		  $(".header-identity-wrapper").addClass("reduce");
-		}
-		else
-		{
-			$(".header-identity-wrapper").removeClass("reduce");
-		}
-	});*/
+  // Dop in a search icon
+  $( ".block-system-main-menu" ).after( $( "<span class='ti-search search'></span>" ) );
+  
+	new WOW().init();
+	
+  // Detect window scroll and update navbar
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 65){
+      $('#l-header').addClass('header-scroll');
+      $(".nav-placeholder").height($(".scrollme").outerHeight() + 35);
+    } else {
+      $(".nav-placeholder").height(0);
+      $('#l-header').removeClass('header-scroll');
+    }
+  });
+  
+  // Smooth scroll for links with .scrollto classes
+  $('.scrollto').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+  
+        if ($('#l-header').length) {
+          top_space = $('#l-header').outerHeight();
+  
+          if (! $('#l-header').hasClass('header-scrolled')) {
+            top_space = top_space - 65;
+          }
+        }
+  
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+  
+        return false;
+      }
+    }
+  });
 
   // Cache the highest
   var highestBox = 0;
@@ -20,8 +49,23 @@ $(function () {
       highestBox = $(this).height(); 
     }
   });
+  
   // Set the height of all those children to whichever was highest 
-  $('.view-display-id-homepage_latest_news_block .views-field-field-main-image').height(highestBox);
-  $(".view-display-id-homepage_latest_news_block .views-field-field-main-image .field-content").css({"position": "absolute", "bottom": "0"});        
-
+  $(window).resize(function() {
+    if(this.resizeTO) clearTimeout(this.resizeTO);
+    this.resizeTO = setTimeout(function() {
+      $(this).trigger('resizeEnd');
+    }, 500);
+  });
+  $(window).bind('resizeEnd', function() {
+    if ($(window).width() >= 768 ){	
+      $('.view-display-id-homepage_latest_news_block .views-field-field-main-image').height(highestBox);
+      $(".view-display-id-homepage_latest_news_block .views-field-field-main-image .field-content").css({"position": "absolute", "bottom": "0"});   
+    } else if ($(window).width() <= 768 ){
+      $('.view-display-id-homepage_latest_news_block .views-field-field-main-image').height('auto');
+      $(".view-display-id-homepage_latest_news_block .views-field-field-main-image .field-content").css({"position": "relative", "bottom": "0"}); 
+    }
+  });
+  $(window).trigger('resize');
+  
 });
--- templates/paragraphs-item--text_block_with_left_floating_he.tpl.php
+++ templates/paragraphs-item--text_block_with_left_floating_he.tpl.php
@@ -28,11 +28,11 @@
 ?>
 <div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
   <div class="content container gutters"<?php print backdrop_attributes($content_attributes); ?>>
-    <div class="col-12 col-sm-4">
+    <div class="col-12 col-sm-4 left">
       <?php  print render($content['field_text_block_title']); ?>
       <?php  print render($content['field_text_block_description']); ?>
     </div>
-    <div class="col-12 col-sm-8">
+    <div class="col-12 col-sm-8 right">
       <?php  print render($content['field_text_block_body']); ?>
     </div>
   </div>
